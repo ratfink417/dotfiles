6497aad89dc5106f8b362c2c435a411f090dae3d
ratfink417 <johnny@kali.kali-local>
2024-09-18 14:13:30 -0700

    streamlined install commands

--- arch/arch_setup.sh
+++ arch/arch_setup.sh
@@ -14,27 +14,27 @@ local_yay_dir=$HOME/.local/yay
 
 # set up yay (for installing AUR pkgs)
 pushd
-sudo pacman -S --needed git base-devel
+sudo pacman -Sy --needed git base-devel
 git clone https://aur.archlinux.org/yay.git $local_yay_dir
 cd $local_yay_dir 
 makepkg -si
 popd
 
 # install utilities that are going to be needed
-sudo pacman -S wget git bat fzf jq unzip lsd 
+sudo pacman -Sy wget git bat fzf jq unzip lsd 
 
 # select a nerdfont to install
 font_choice='IBMPlexMono.zip'
 
 # install fontconfig and install a nerd font
-sudo pacman -S fontconfig 
+sudo pacman -Sy fontconfig 
 wget -O /tmp/font.zip \
 https://raw.githubusercontent.com/ryanoasis/nerd-fonts/releases/latest/download/$font_choice
 unzip /tmp/font.zip -d $local_fonts_dir
 fc-cache -f -v
 
 # install zsh and make it the default shell
-sudo pacman -S zsh 
+sudo pacman -Sy zsh 
 chsh -s /usr/bin/zsh $USER
 
 # install starship and configure zsh
