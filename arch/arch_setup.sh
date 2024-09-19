ca60b17070b4a5ed73ca6f008f2e67fc0d944526
ratfink417 <johnny@kali.kali-local>
2024-09-19 10:43:59 -0700

    added --noconfirm to package installs

--- arch/arch_setup.sh
+++ arch/arch_setup.sh
@@ -15,14 +15,14 @@ local_yay_dir=$HOME/.local/yay
 
 # set up yay (for installing AUR pkgs)
 pushd
-sudo pacman -Sy --needed git base-devel
+sudo pacman -Sy --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git $local_yay_dir
 cd $local_yay_dir 
 makepkg -si
 popd
 
 # install utilities that are going to be needed
-sudo pacman -Sy wget git bat fzf jq unzip lsd man-db fd ripgrep python3 lua51 luarocks eslint_d rust composer prettier nodejs npm jdk-openjdk openjdk-doc python-virtualenv perl python-black tree-sitter julia python-pip python-neovim ruby lsd
+sudo pacman -Sy --noconfirm wget git bat fzf jq unzip lsd man-db fd ripgrep python3 lua51 luarocks eslint_d rust composer prettier nodejs npm jdk-openjdk openjdk-doc python-virtualenv perl python-black tree-sitter julia python-pip python-neovim ruby lsd
 
 # configure nodejs and npm for local user 
 npm set prefix="$HOME/.local"
@@ -35,13 +35,13 @@ sudo mandb
 font_choice='IBMPlexMono.zip'
 
 # install fontconfig and install a nerd font
-sudo pacman -Sy fontconfig 
+sudo pacman -Sy --noconfirm fontconfig 
 wget -O /tmp/font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IBMPlexMono.zip
 unzip /tmp/font.zip -d $local_fonts_dir
 fc-cache -f -v
 
 # install zsh and make it the default shell
-sudo pacman -Sy zsh 
+sudo pacman -Sy --noconfirm zsh 
 chsh -s /usr/bin/zsh $USER
 
 # install starship and configure zsh
@@ -52,11 +52,11 @@ cat ./zshrc_append >> $HOME/.zshrc
 cp ../starship/starship.toml $HOME/.config/starship.toml
 
 # install neovim and copy the config folder
-sudo pacman -S neovim
+sudo pacman -S --noconfirm neovim
 cp -R ../neovim $HOME/.config/nvim
 
 # install and configure tmux
-sudo pacman -S tmux 
+sudo pacman -S --noconfirm tmux 
 yay -S tmux-plugin-manager
 cp -R ../tmux/.tmux $HOME
 cp ../tmux/.tmux.conf $HOME
